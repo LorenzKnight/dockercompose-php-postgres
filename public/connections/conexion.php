f84b076fb25cae0b1f2d8f240d3677d8fff5cd29
Lorenz Knight <joellorenzo.k@gmail.com>
2022-10-20 19:38:49 +0200

    updates

--- public/connections/conexion.php
+++ public/connections/conexion.php
@@ -1,10 +1,18 @@
 <?php
-  $appName = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-  $connStr = "host=localhost port=5432 dbname=ratedb user=postgres password=Admin456 ";
+$conn_host = ' host=127.0.0.1';
+$conn_port = ' port=5432';
+$conn_dbname = ' dbname=ratedb';
+$conn_user = ' user=admin';
+$conn_pass = ' password=Admin456';
 
+$db = pg_connect($conn_host . $conn_port . $conn_dbname . $conn_user . $conn_pass);
+if ($db == false) {
+  echo "sql connection error!";
+  exit();
+}
 
-  //simple check
-  $conn = pg_connect($connStr);
-  $result = pg_query($conn, "select * from users");
-  var_dump(pg_fetch_all($result));
+//simple check
+// $conn = pg_connect($connStr);
+// $result = pg_query($conn, "select * from users");
+// var_dump(pg_fetch_all($result));
 ?>
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e0cd95f2ad3eb2bb18e02395b5e4ba3fab126b4a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e0cd95f2ad3eb2bb18e02395b5e4ba3fab126b4a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
